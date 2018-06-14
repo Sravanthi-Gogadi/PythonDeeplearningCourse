90cf9c4a183bd9e9b9eb1ce6d2b5bc70f8e8f3ce
Sravanthi-Gogadi <sravanthigogadi@gmail.com>
2018-06-14 12:41:17 -0500

    ICP4

--- ICP4/num.py
+++ ICP4/num.py
@@ -2,6 +2,7 @@
 import numpy as np
 # Define the range of the matrix and reshape them
 ip=np.arange(100).reshape(10,10)
+# print the matrix
 print(ip)
 for i in range(10):
  # print the max and min values of each row in the numpy array
