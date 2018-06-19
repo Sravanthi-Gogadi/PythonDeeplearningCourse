904c99b8bee85b9a6dd44a340ef191e8790064c0
Sravanthi-Gogadi <sravanthigogadi@gmail.com>
2018-06-19 18:27:46 -0500

    ICP5

--- ICP5/LR.py
+++ ICP5/LR.py
@@ -1,13 +1,22 @@
+# import the required library numpy and matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-
+# Given the features X and Y
 x=np.array([0,1,2,3,4,5,6,7,8,9])
 y=np.array([1,3,2,5,7,8,8,9,10,12])
+# calculate the mean of X
 xMean=np.mean(x)
+# calculate the mean of y
 yMean=np.mean(y)
+# to calculate the coefficient of equation y = mx + c where B1 is the slope
 B1=(np.sum((x-xMean)*(y-yMean)))/(np.sum(np.power((x-xMean),2)))
+# calculate the y intercept of the linear equation y=mx+c where c is intercept of y
 B0=yMean-(B1*xMean)
+# plot the x and y values
 plt.scatter(x,y)
+# linear regression equation with b1 as slope and b0 as y intercept
 ry=B1*x+B0
+# plot the line that best separates the data points
 plt.plot(x,ry)
+# show the plot for the linear regression model
 plt.show()
--- ICP5/kmeans.py
+++ ICP5/kmeans.py
@@ -1,19 +1,28 @@
+# import matplotlib for plotting the clusters
 import matplotlib.pyplot as plt
+# import numpy library
 import numpy as np
+# from sklearn import kmeans
 from sklearn.cluster import KMeans
-X = np.array([[5.1,60],
-     [5.2,65],
-     [5.3,68],
-     [5.4,70],
-     [5.5,75],
-     [5.6,78],
-     [5.7,80],
-     [5.8,85],
-     [5.9,90],
-     [6.0,91],])
+# import random package to take random input dynamically
+import random
+# set the seed value to any int to get the same random values
+random.seed(9)
+# use uniform function to get the float values within range(4,7)
+height=[round(random.uniform(4,7),2) for i in range(20)]
+# use uniform function to get the float values within range(60,80)
+weight=[round(random.uniform(60,80),2) for i in range(20)]
+# numpy function vstack to stack the two arrays vertically
+X = np.vstack((height,weight)).T
+# print the 2d array
+print(X)
+# define the model and pass in number of clusters
 kmeans = KMeans(n_clusters=3)
+# fit the model to the array X
 kmeans.fit(X)
-print(kmeans.labels_)
+# Scatter plot the datapoints with different colors assigned to various clusters
 plt.scatter(X[:,0], X[:,1], c=kmeans.labels_, cmap='rainbow')
+# Scatter plot the centroids with black color
 plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], color='black')
+# show the plot using matplotlib package
 plt.show()
